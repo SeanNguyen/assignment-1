0e05b1b81e497a1a1cfe58c4e1eb610b4afafec9
SeanNguyen <firestormkjc.1993@gmail.com>
2014-08-23 04:00:01 +0800

    Updated GUI

--- Source/src/control/Configurations.java
+++ Source/src/control/Configurations.java
@@ -2,17 +2,17 @@ package control;
 
 public class Configurations {
 	public static final String GETRESULT_BUTTON_TEXT = "GET RESULT";
-	public static final String TITLE_DEFAULTTEXT = "Enter movie tittles here !";
-	public static final String IGNOREDWORD_DEFAULTTEXT = "Enter ignored words here !";
-	public static final String RESULT_DEFAULTTEXT = "Results will be shown here !";
+	public static final String TITLE_DEFAULTTEXT = "Enter movie tittles here: ";
+	public static final String IGNOREDWORD_DEFAULTTEXT = "Enter ignored words here: ";
+	public static final String RESULT_DEFAULTTEXT = "Results will be shown here: ";
 	
-	public static final int MAINFRAME_HEIGHT = 700;
+	public static final int MAINFRAME_HEIGHT = 500;
 	public static final int MAINFRAME_WIDTH = 1300;
 	
-	public static final int INPUTTEXT_COLSIZE = 50;
+	public static final int INPUTTEXT_COLSIZE = 20;
 	public static final int INPUTTEXT_ROWSIZE = 20;
 	
-	public static final int RESULTTEXT_COLSIZE = 50;
+	public static final int RESULTTEXT_COLSIZE = 20;
 	public static final int RESULTTEXT_ROWSIZE = 20;
 	
 	
--- Source/src/view/KwicView.java
+++ Source/src/view/KwicView.java
@@ -1,14 +1,17 @@
 package view;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionListener;
-
 import control.Configurations;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
 
 
 public class KwicView extends JFrame{
@@ -17,19 +20,23 @@ public class KwicView extends JFrame{
 	JTextArea ignoreWordsText = new JTextArea(Configurations.INPUTTEXT_ROWSIZE, Configurations.INPUTTEXT_COLSIZE);
 	JTextArea resultText = new JTextArea(Configurations.RESULTTEXT_ROWSIZE, Configurations.RESULTTEXT_COLSIZE);
 	JButton getResultButton = new JButton (Configurations.GETRESULT_BUTTON_TEXT);
+	JLabel inputLabel = new JLabel(Configurations.TITLE_DEFAULTTEXT);
+	JLabel ignoredWordLabel = new JLabel(Configurations.IGNOREDWORD_DEFAULTTEXT);
+	JLabel resultLabel = new JLabel(Configurations.RESULT_DEFAULTTEXT);
 	
 	public KwicView() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(Configurations.MAINFRAME_WIDTH, Configurations.MAINFRAME_HEIGHT);
+		
+		mainPanel.add(inputLabel);
 		mainPanel.add(titleText);
+		mainPanel.add(ignoredWordLabel);
 		mainPanel.add(ignoreWordsText);
-		mainPanel.add(getResultButton);
+		mainPanel.add(resultLabel);
 		mainPanel.add(resultText);
+		mainPanel.add(getResultButton);
 		this.add(mainPanel);
 		
-		titleText.setText(Configurations.TITLE_DEFAULTTEXT);
-		ignoreWordsText.setText(Configurations.IGNOREDWORD_DEFAULTTEXT);
-		resultText.setText(Configurations.RESULT_DEFAULTTEXT);
 	}
 	
 	public String GetTitleText () {
