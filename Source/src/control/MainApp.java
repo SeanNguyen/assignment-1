caac463472050fa06cb1218112ca0f3ba6ced193
SeanNguyen <firestormkjc.1993@gmail.com>
2014-08-30 11:05:21 +0800

    change App name

--- Source/src/control/MainApp.java
+++ Source/src/control/MainApp.java
@@ -17,7 +17,7 @@ public class MainApp extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("AddressApp");
+        this.primaryStage.setTitle(Configurations.APP_TITTLE);
 
         try {
             // Load the root layout from the fxml file
